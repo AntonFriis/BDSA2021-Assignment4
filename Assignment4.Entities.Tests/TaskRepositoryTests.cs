76a04d24d1f400ee601687d1ac8cae473f4cd5bc
malj <malj@itu.dk>
2021-10-06 18:06:38 +0200

    Complete resstructure of code to conform to the new additions.

--- Assignment4.Entities.Tests/TaskRepositoryTests.cs
+++ Assignment4.Entities.Tests/TaskRepositoryTests.cs
@@ -16,39 +16,29 @@ namespace Assignment4.Entities.Tests
         [Fact]
         public void TaskAll_Returns3Tasks()
         {
-            using var context = new DbContextFactory().CreateDbContext();
-            var tasks = new TaskRepository(context).All();
-            Assert.Equal(3, tasks.Count);
+
         }
-        
+
         [Fact]
         public void TaskCreate_ReturnsId4()
         {
-            using var context = new DbContextFactory().CreateDbContext();
-            var task = new TaskDTO {
-                Title = "Test task",
-                Description = "Task for testing",
-                State = Closed};
-            Assert.Equal(4, new TaskRepository(context).Create(task));
+
         }
-        
+
         [Fact]
         public void TaskDelete_ReturnsCountOf2()
         {
-            using var context = new DbContextFactory().CreateDbContext();
-            new TaskRepository(context).Delete(2);
-            Assert.Equal(2, context.Tasks.Count());
+
         }
-        
+
         [Fact]
         public void TaskFindById_ReturnsTaskTitleImplement_FromTaskId1()
         {
-            using var context = new DbContextFactory().CreateDbContext();
-            Assert.Equal("Implement ITaskRepository", new TaskRepository(context).FindById(1).Title);
+
         }
-        
-        
-        
+
+
+
         // public void Update(TaskDTO task)
         // {
         // }
--- Assignment4.Entities/TaskRepository.cs
+++ Assignment4.Entities/TaskRepository.cs
@@ -1,83 +1,58 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System;
 using Assignment4.Core;
+using static Assignment4.Core.Response;
 
 namespace Assignment4.Entities
 {
     public class TaskRepository : ITaskRepository
     {
         private readonly KanbanContext _context;
-        
+
         public TaskRepository(KanbanContext context)
         {
             _context = context;
         }
-        
+
+        public (Response Response, int TaskId) Create(TaskCreateDTO task)
+        {
+            return (BadRequest, 0);
+        }
+        public IReadOnlyCollection<TaskDTO> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+        public IReadOnlyCollection<TaskDTO> ReadAllRemoved()
+        {
+            throw new NotImplementedException();
+        }
+        public IReadOnlyCollection<TaskDTO> ReadAllByTag(string tag)
+        {
+            throw new NotImplementedException();
+        }
+        public IReadOnlyCollection<TaskDTO> ReadAllByUser(int userId)
+        {
+            throw new NotImplementedException();
+        }
+        public IReadOnlyCollection<TaskDTO> ReadAllByState(State state)
+        {
+            throw new NotImplementedException();
+        }
+        public TaskDetailsDTO Read(int taskId)
+        {
+            throw new NotImplementedException();
+        }
+        public Response Update(TaskUpdateDTO task)
+        {
+            throw new NotImplementedException();
+        }
+        public Response Delete(int taskId)
+        {
+            throw new NotImplementedException();
+        }
+
         public void Dispose() => _context.Dispose();
-
-        public IReadOnlyCollection<TaskDTO> All()
-        {
-            var tasks = new List<TaskDTO>();
-            foreach (var task in _context.Tasks)
-            {
-                tasks.Add(new TaskDTO
-                {
-                    Id = task.Id,
-                    Title = task.Title,
-                    Description = task.Description,
-                    AssignedToId = task.AssignedTo.Id,
-                    Tags = task.Tags.Select(tag => tag.Name).ToImmutableList(),
-                    State = task.State
-                });
-            }
-
-            return tasks.ToImmutableList();
-        }
-
-        public int Create(TaskDTO task)
-        {
-            _context.Tasks.Add(new Task
-            {
-                Id = task.Id,
-                Title = task.Title,
-                AssignedTo = _context.Users.FirstOrDefault(user => user.Id == task.AssignedToId),
-                Description = task.Description,
-                State = task.State,
-                Tags = _context.Tags.Where(tag => tag.Name == task.Tags.Select(name => name).FirstOrDefault()).ToList()
-            });
-            return _context.SaveChanges();
-        }
-
-        public void Delete(int taskId)
-        {
-            var taskToRemove = _context.Tasks.Where(task => task.Id == taskId).Select(task => task).FirstOrDefault();
-            if (taskToRemove != null) _context.Tasks.Remove(taskToRemove);
-            _context.SaveChanges();
-        }
-
-        public TaskDetailsDTO FindById(int id)
-        {
-            var task = _context.Tasks.FirstOrDefault(t => t.Id == id);
-            
-            return new TaskDetailsDTO
-                {
-                    Id = task.Id,
-                    Title = task.Title,
-                    Description = task.Description,
-                    AssignedToId = task.AssignedTo.Id,
-                    AssignedToName = task.AssignedTo.Name,
-                    AssignedToEmail = task.AssignedTo.Email,
-                    Tags = task.Tags.Select(tag => tag.Name),
-                    State = task.State
-                };
-            
-        }
-
-        public void Update(TaskDTO task)
-        {
-            _context.Update(task);
-            _context.SaveChanges();
-        }
     }
 }
